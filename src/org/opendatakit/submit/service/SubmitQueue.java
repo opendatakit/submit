3c1b6d0566aab9e6f565beb33b4b29a612405981
morgan a vigil <morgan.a.vigil@gmail.com>
2013-09-10 21:38:37 -0700

    Adding unadded classes.

---
+++ src/org/opendatakit/submit/service/SubmitQueue.java
@@ -0,0 +1,130 @@
+package org.opendatakit.submit.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import org.opendatakit.submit.data.DataObject;
+import org.opendatakit.submit.data.SendObject;
+import org.opendatakit.submit.data.SubmitObject;
+
+public class SubmitQueue {
+	// TODO reimagine some of these data structures
+	private LinkedList<SubmitObject> mSubmitQueue = null; // Keeps track of all SubmitObjects and keeps them in a FIFO queue
+	private HashMap<String, ArrayList<String>> mAppIdToSubmitIds = null; // Keeps track of all SubmitObjects that belong to a given app. Maps list of SubmitIDs to an AppID.
+	private HashMap<String, SubmitObject> mSubmitIdToSubmitObject = null;
+	
+	/**
+	 * Empty constructor
+	 */
+	public SubmitQueue() {
+		mSubmitQueue = new LinkedList<SubmitObject>();
+		mAppIdToSubmitIds = new HashMap<String, ArrayList<String>>();
+		mSubmitIdToSubmitObject = new HashMap<String, SubmitObject>();
+	}
+	
+	public LinkedList<SubmitObject> getSubmitObjectQueue() {
+		return mSubmitQueue;
+	}
+	
+	public int getSubmitQueueSize() {
+		return mSubmitQueue.size();
+	}
+	
+	public LinkedList<SubmitObject> getSubmitQueue() {
+		return mSubmitQueue;
+	}
+	
+	public boolean onSubmitQueue(String submit_id) {
+		return mSubmitIdToSubmitObject.containsKey(submit_id);
+	}
+	
+	public void updateSubmitQueue(SubmitObject submit) {
+		
+		for(SubmitObject sub : mSubmitQueue) {
+			if(submit.getSubmitID().equals(sub.getSubmitID())) {
+				removeSubmitObjectFromQueue(sub);
+				addSubmitObjectToQueue(submit);
+			}
+		}
+	}
+	
+	public SubmitObject getSubmitObjectBySubmitId(String submit_id) {
+		return mSubmitIdToSubmitObject.get(submit_id);
+	}
+	
+	public ArrayList<String> getSubmitIdsByAppId(String app_id) {
+		return mAppIdToSubmitIds.get(app_id);
+	}
+	
+	public void addSubmitObjectLast(SubmitObject submit) {
+		mSubmitQueue.addLast(submit);
+		addSubmitIdToAppIdMap(submit);
+		mSubmitIdToSubmitObject.put(submit.getSubmitID(), submit);
+	}
+	
+	public SubmitObject popTopSubmitObject() {
+		SubmitObject top = mSubmitQueue.getFirst();
+		removeSubmitObjectFromQueue(top);
+		return top;
+	}
+	
+	public void addSubmitObjectToQueue(SubmitObject submit) {
+		mSubmitQueue.add(submit);
+		addSubmitIdToAppIdMap(submit);
+		mSubmitIdToSubmitObject.put(submit.getSubmitID(), submit);
+	}
+	
+	public void removeSubmitObjectFromQueue(SubmitObject submit) {
+		mSubmitQueue.remove(submit);
+		removeSubmitIdFromAppIdMap(submit.getAppID(), submit.getSubmitID());
+	}
+	
+	/*
+	 * This second signature for removeSubmitObjectFromQueue is more
+	 * time intensive than the other, however, it allows an Application
+	 * to remove a SubmitObject from the SubmitQueue with only an 
+	 * ApplicationID and SubmitID.
+	 */
+	public void removeSubmitObjectFromQueue(String app_id, String submit_id) {
+		for (SubmitObject submit : mSubmitQueue) {
+			// Go through each SubmitObject on mSubmitQueue
+			// and compare the SubmitIDs
+			if (submit_id.equals(submit.getSubmitID())) {
+				mSubmitQueue.remove(submit);
+			}
+		}
+		removeSubmitIdFromAppIdMap(app_id, submit_id);
+		mSubmitIdToSubmitObject.remove(submit_id);
+	}
+
+	/* Private */
+	private void addSubmitIdToAppIdMap(SubmitObject submit) {
+		ArrayList<String> submitids = mAppIdToSubmitIds.get(submit.getAppID());
+		if (submitids == null) {
+			submitids = new ArrayList<String>();
+		}
+		submitids.add(submit.getSubmitID());
+		mAppIdToSubmitIds.put(submit.getAppID(), submitids);
+	}
+	
+	/*
+	 * Note that the signature takes an AppId and SubmitId because
+	 * these are two pieces of information an Application can 
+	 * provide about a SubmitObject. For the delete() call used in
+	 * the SubmitServiceInterface, we need this functionality.
+	 */
+	private void removeSubmitIdFromAppIdMap(String app_id, String submit_id) {
+		ArrayList<String> submitids = mAppIdToSubmitIds.get(app_id);
+		if (submitids != null) {
+			submitids.remove(submit_id);
+		}
+		mAppIdToSubmitIds.put(app_id, submitids);
+	}
+
+	/**
+	 * Generated serialVersionUID
+	 */
+	private static final long serialVersionUID = 2742367162623177899L;
+	
+}
---
+++ src/org/opendatakit/submit/service/SubmitServiceInterface.java
@@ -0,0 +1,127 @@
+package org.opendatakit.submit.service;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.opendatakit.submit.data.DataObject;
+import org.opendatakit.submit.data.SendObject;
+import org.opendatakit.submit.data.SubmitObject;
+
+import android.content.Context;
+import android.os.RemoteException;
+import android.util.Log;
+
+/**
+ * Implementation of ClientRemote Stub for 
+ * Applications to interface with SubmitService
+ * @author mvigil
+ *
+ */
+public class SubmitServiceInterface extends ClientRemote.Stub{
+	
+	private final String TAG = SubmitServiceInterface.class.getName();
+	
+	/* Fields that correspond with SubmitService */
+	private SubmitService mService = null;
+	private Context mContext = null;
+	
+	public SubmitServiceInterface(SubmitService service, Context context) {
+		mService = service;
+		mContext = context;
+	}
+	
+	@Override
+	public String submit(String app_uid, DataObject data, SendObject send)
+			throws RemoteException {
+		
+		Log.i(TAG, "In submit()");
+		SubmitObject submit = new SubmitObject(app_uid, data, send);
+
+		mService.addSubmitObject(submit);
+		//mService.manageQueue();
+		return submit.getSubmitID();
+	}
+	
+	@Override
+	public String register(String app_uid, DataObject data)
+			throws RemoteException {
+		Log.i(TAG, "In register()");
+				
+		SubmitObject submit = new SubmitObject(app_uid, data, null);		
+		if (submit == null) {
+			Log.e(TAG, "!!!!!!!!SubmitObject is null!!!!!!!!");
+		}
+		/* Map submission ID to DataObject */
+		// Here, we assume that the SubmitID
+		// is unique, so we do not check before
+		// putting it into the map
+
+		mService.addSubmitObject(submit);
+		//mService.manageQueue();
+		return submit.getSubmitID();
+	}
+	
+	@Override
+	public int queueSize() throws RemoteException {
+		Log.i(TAG, "In queueSize()");
+		return mService.getSubmitQueueSize();
+	}
+	
+	@Override
+	public boolean onQueue(String submit_uid) throws RemoteException {
+		Log.i(TAG, "In onQueue()");
+		return mService.onSubmitQueue(submit_uid);	
+	}
+	
+	@Override
+	public SendObject getSendObjectById(String submit_uid) throws RemoteException {
+		return mService.getSubmitObjectFromSubmitQueue(submit_uid).getAddress();
+	}
+	
+	@Override
+	public String[] getQueuedSubmissions(String app_uid) throws RemoteException {
+		String[] ids = null;
+		ArrayList<String> idlist = mService.getSubmitIdsFromAppId(app_uid);
+		
+		// If there are no SubmitObjects belonging to
+		// the AppID, then return null values
+		if(idlist.size() < 1) {
+			return null;
+		}
+		
+		// Transfer listed SubmitIDs to 
+		// String[] ids. We do the transfer
+		// for the purpose of serialization
+		ids = new String[idlist.size()];
+		int position = 0;
+		for(String id : idlist) {
+			ids[position] = id;
+			position++;
+		}
+		return ids;
+	}
+	
+	@Override
+	public DataObject getDataObjectById(String submit_uid) throws RemoteException {
+		return mService.getSubmitObjectFromSubmitQueue(submit_uid).getData();
+	}
+	
+	@Override
+	public void delete(String submit_uid )
+			throws RemoteException {
+		Log.i(TAG, "In delete()");
+		
+	}
+
+	@Override
+	public String registerApplication(String app_uid)
+			throws RemoteException {
+		
+		UUID uid = UUID.randomUUID();
+		
+		String appUid = uid.toString();
+		AppReceiver listener = new AppReceiver(appUid, mService, mContext);
+		mService.addAppReceiver(listener);
+		return appUid;
+	}
+}
